9281f4c9b89e9b22df25128132f297576ed99ebd
Wesley van der Molen <v0ilawes@gmail.com>
2018-12-19 15:04:45 +0100

    beschikbaarheid

--- beschikbaarheid.php
+++ beschikbaarheid.php
@@ -7,8 +7,10 @@
   <title>Titel</title>
   <link rel="stylesheet" href="assets/css/bulma.min.css">
   <link rel="stylesheet" href="assets/css/styles.css">
+  <link rel="stylesheet" href="assets/css/google.css">
   <link rel="shortcut icon" type="image/png" href="assets/img/favicon.png">
   <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
+
 
   <meta name="description" content="Titel">
   <meta property="og:type" content="website">
